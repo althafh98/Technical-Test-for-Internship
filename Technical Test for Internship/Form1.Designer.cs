8108b214fe941f31f112b613232fc216c80a1316
althafh98 <althafhusain08@gmail.com>
2020-11-19 00:06:02 +0530

    First Commit

---
+++ Technical Test for Internship/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Technical_Test_for_Internship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(487, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(391, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rice Quantity (Kg)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(672, 295);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(116, 37);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(584, 258);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(204, 31);
+            this.txtInput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculate Lowest Quantity ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Technical Test for Internship/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Technical_Test_for_Internship
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            float OrderAmount = float.Parse(txtInput.Text); 
+            int OrderAmountInput = 0;
+            float Stock;
+            OrderAmountInput = (int)OrderAmount;
+            Stock = OrderAmount - OrderAmountInput;
+
+            int i; 
+            int j;
+            int m;
+            int k;
+
+            if (Stock == 0.5 || Stock == 0) {
+                {
+                    MessageBox.Show("Your Order is Success" , "Success");
+                }
+                for (i = 0; OrderAmount >= 4; i++)
+                {
+                    OrderAmount = OrderAmount - 4;
+                }
+
+                for (j = 0; OrderAmount >= 2; j++)
+                {
+                    OrderAmount = OrderAmount - 2;
+                }
+
+                for (m = 0; OrderAmount >= 1; m++)
+                {
+                    OrderAmount = OrderAmount - 1;
+                }
+
+                for (k = 0; OrderAmount == 0.5; k++)
+                {
+                    OrderAmount = (float)(OrderAmount - 0.5);
+                }
+                label2.Text = "4Kg Bags :" + " " + i.ToString() + "\r\n" + "2Kg Bags :" + " " + j.ToString() + "\r\n" + "1Kg Bags :" + " " + m.ToString() + "\r\n" + "1Kg Bags Because 0.5Kg Bags Not Available :" + " " +  k.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Your Order is Failed" , "Failed");
+                label2.Text = " ";
+            }
+        }
+
+    }
+}
+
